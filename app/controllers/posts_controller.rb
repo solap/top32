7f2ab4ebff360b694a9ef4a265590944f10aa810
Joel Dehlin <joel.dehlin@JoelDehlinI26829.local>
2012-12-09 16:04:14 +0100

    re-commit. looks like it didn't work.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -86,34 +86,60 @@ class PostsController < ApplicationController
   end
 
   def do_web_thing
+    lolking = "http://www.lolking.net"
     @posts = Post.all
+    #@posts = @posts.order("team_name desc")
+
+    #@posts = @posts.sort_by!{|post| post.team_elo}.reverse
     @posts.each do |post|
-      urlEncodedString = CGI::escape(post.name)
-      baseUrl = "http://www.lolking.net"
-      url = baseUrl + "/search?name=#{urlEncodedString}"
-      doc = Nokogiri::HTML(open(url))
-      doc.xpath("//div[@class='search_result_item']").each do |resultItem|
+      #encode the string to deal with spaces
+      encoded_summoner_name = CGI::escape(post.name)
+      summoner_summary_page = lolking + "/search?name=#{encoded_summoner_name}"
+      summoner_summary_page_html = Nokogiri::HTML(open(summoner_summary_page))
+      summoner_summary_page_html.xpath("//div[@class='search_result_item']").each do |resultItem|
         onclick = resultItem['onclick']
+
         if onclick.include? 'summoner/na'
           index = onclick.index("'") + 1
           link = onclick[index..onclick.length-1]
           index = link.index("'")-1
-          goToUrl = baseUrl + link[0..index]
-          doc2 = Nokogiri::HTML(open(goToUrl))
-          detail = doc2.at_css("title").text
-          diversity = doc2.at_css("ul.personal_ratings li:last div:nth-child(2)").text
-          points = doc2.at_css("ul.personal_ratings li:last div:nth-child(7) span").text
-          #puts detail.inspect
-          #puts points.to_i
+          summoner_profile = lolking + link[0..index]
+          summoner_profile_html = Nokogiri::HTML(open(summoner_profile))
+          #puts "Nokogiri::HTML(open(summoner_profile))"
+          team_name = summoner_profile_html.at_css("ul.personal_ratings li:last div:nth-child(2)").text
+          #puts "finished team name"
+          #puts "team_name ********* " + team_name
+
+
+          if team_name != ""
+
+            points = summoner_profile_html.at_css("ul.personal_ratings li:last div:nth-child(7) span").text
+
+            #puts "finished team_elo"
+            detail = summoner_profile_html.at_css("title").text
+          end
+          #puts "title: " + detail.inspect.to_i
+          #puts "team elo: " + points.to_i
+          #puts "before post.name"
           post.name = post.name
-          post.team_name = diversity.inspect
+          #puts "post.name"
+          post.team_name = team_name.inspect
+          #puts "post.team_name"
           post.team_elo = points.to_i
-          puts post.team_name + " " + post.team_elo.to_s
+          #puts "post.team_elo"
+          puts post.name + " " + post.team_name + " " + post.team_elo.to_s
           post.save
+          #puts "post.save"
         end
+
       end
+      #puts "out summoner_summary_page_html.xpath"
     end
-    @posts = @posts.order(team_elo).reverse
+
+    @posts = @posts.order("team_elo desc")
     render "index"
+  end
+  def yadda
+
   end
 end
