efb05c4ab8074169005e3407cfcd24625e9519b4
Joel Dehlin <joel.dehlin@gmail.com>
2012-12-13 06:39:26 +0100

    add simple db to heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	attr_accessible :name, :team_elo, :team_name
 	include TeamBuilder
   	def self.create_team_list
-=begin
+
 	    Post.all.each do |post|
 	    	new_summoners = post.find_summoner_opponents
 	    	new_summoners.each do |opponent_name|
@@ -13,7 +13,7 @@ class Post < ActiveRecord::Base
 	    	end
 	      #encode the string to deal with spaces
 	    end
-=end
+
 	    Post.all.each do |post|
 	    	details = post.get_team_details
 	    	post.update_attributes(details)
