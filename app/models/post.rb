d676fa4f70d853c046736b727bd40e0dd2b7953b
Joel Dehlin <joel.dehlin@gmail.com>
2012-12-14 06:45:04 +0100

    comment out get_summoners, add if check, add get teams

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,23 +4,25 @@ class Post < ActiveRecord::Base
 	include TeamBuilder
   	def self.create_team_list
 
+=begin
 	    Post.all.each do |post|
 	    	new_summoners = post.find_summoner_opponents
-	    	new_summoners.each do |opponent_name|
-	    		if Post.find_by_name(opponent_name)
-	    			Post.create(:name => opponent_name)
-	    		end
+	    	if new_summoners.any?
+		    	new_summoners.each do |opponent_name|
+		    		if Post.find_by_name(opponent_name)
+		    			Post.create(:name => opponent_name)
+		    		end
+		    	end
 	    	end
 	      #encode the string to deal with spaces
 	    end
-=begin
+=end
 	    Post.all.each do |post|
 	    	details = post.get_team_details
 	    	post.update_attributes(details)
 
 	    end
 	    #binding.pry
-=end
 	end
 
 
