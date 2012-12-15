a9e50c8bfaf580798dce4e7f6e94308325efcadc
Joel Dehlin <joel.dehlin@gmail.com>
2012-12-15 07:40:01 +0100

    removed summoner variable check from team details function call.

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 	    	puts "NAME BEFORE FUNCTION CALL: #{name}"
 	    	new_summoners = post.find_summoner_opponents if !name.blank?
 	    	puts "new_summoners AFTER FUNCTION CALL: #{new_summoners.to_s}"
-	    	if !new_summoners==nil then
+	    	if new_summoners!=nil then
 	    		puts "NEW_SUMMONERS BEFORE NEW_SUMMONERS.EACH: #{new_summoners.to_s}"
 		    	new_summoners.each do |opponent_name|
 		    		if !Post.exists?(name: opponent_name)
@@ -20,19 +20,15 @@ class Post < ActiveRecord::Base
 	    	end
 	      #encode the string to deal with spaces
 	    end
-	    puts "IN BETWEEN FUNCTIONS: #{new_summoners.to_s}"
-	    if !new_summoners.empty?
-	    	puts "IN NEW SUMMONERS.ANY: #{new_summoners.to_s}"
-		    Post.all.each do |post|
-		    	if !post.name.blank?  then
-			    	puts "SUMMONER NAME: #{post.name}"
-			    	details = post.get_team_details
-			  		puts "DETAILS: #{details}"
-			    	post.update_attributes(details)
-			    	puts "POST: #{post.inspect.to_s}"
-		    	end
-		    end
-		end
+	    Post.all.each do |post|
+	    	if !post.name.blank?  then
+		    	puts "SUMMONER NAME: #{post.name}"
+		    	details = post.get_team_details
+		  		puts "DETAILS: #{details}"
+		    	post.update_attributes(details)
+		    	puts "POST: #{post.inspect.to_s}"
+	    	end
+	    end
 	    #binding.pry
 	end
 
