6c44a971af6479cfe97aeabdbde1566830d982de
Joel Dehlin <joel.dehlin@gmail.com>
2012-12-22 21:48:52 +0100

    added name to view

---
+++ spec/is_eligible_spec.rb
@@ -0,0 +1,22 @@
+require "is_eligible" #loading the file with a pre-pended lib
+
+describe "is_eligible" do
+
+  it "returns false if passed in a string which is one of the three" do
+    #expect(Gol.new.board).to be_kind_of Board
+    yadda = is_eligible("Team Solo Mid")
+    yadda.should eq(false)
+  end
+
+  it "returns true if passed in a string which is not one of the three" do
+    #expect(Gol.new.board).to be_kind_of Board
+    yadda = is_eligible("yadda")
+    yadda.should eq(true)
+  end
+
+  it "returns nil if passed in a non-string" do
+    yadda = is_eligible(45)
+    yadda.should eq(nil)
+  end
+
+end
