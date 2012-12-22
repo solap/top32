3afca70060520fa5c8bd7963272aa4769315859d
Joel Dehlin <joel.dehlin@gmail.com>
2012-12-23 00:15:40 +0100

    fix return again

--- spec/lib/team_builder_spec.rb
+++ spec/lib/team_builder_spec.rb
@@ -2,13 +2,21 @@ require "spec_helper"
 
 describe TeamBuilder do
   describe "get_team_details" do
-
     it "returns 'none' when player has no team" do
         #post = Post.new
         post = FactoryGirl.create(:post, name: "yadda")
         #post.name = "yadda"
         yadda = post.get_team_details
         yadda.should eq("none")
+    end
+    it "returns details[] when player has a team" do
+        #post = Post.new
+        post = FactoryGirl.create(:post, name: "tamtar")
+        #post.name = "yadda"
+        yadda = post.get_team_details
+        yadda[:name].should eq("tamtar")
+        yadda[:team_name].should #how do i say "not be empty?"
+
     end
   end
 end
