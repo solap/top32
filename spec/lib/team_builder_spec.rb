5808ac021191553522f5bedce82dbf10f5f7b00d
Joel Dehlin <joel.dehlin@gmail.com>
2012-12-22 23:21:32 +0100

    fixed some random stuff

--- spec/lib/team_builder_spec.rb
+++ spec/lib/team_builder_spec.rb
@@ -1,11 +1,13 @@
-require "team_builder" #loading the file with a pre-pended lib
+require "spec_helper.rb"
 
-describe "get_team_details" do
+describe "team_builder" do
+  describe "get_team_details" do
 
-  it "returns 'none' when player has no team" do
-      post = Post.new
-      post.name = "yadda"
-      details = post.get_team_details
-      yadda.should eq("none")
+    it "returns 'none' when player has no team" do
+        post = Post.new
+        post.name = "yadda"
+        yadda = post.get_team_details
+        yadda.should eq("none")
+    end
   end
 end
